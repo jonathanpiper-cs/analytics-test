5586e1f91e9b8b7310c07cd5520276acb4517b5d
Jonathan Piper <jonathan.piper@contentstack.com>
2025-05-29 21:33:57 -0700

    ticky

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,17 @@
-   // app/api/auth/[...nextauth]/route.ts
-   import NextAuth from 'next-auth';
-   import { authConfig } from '@/auth.config';
-   import Google from 'next-auth/providers/google';
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth'
+import { authConfig } from '@/auth.config'
+import Google from 'next-auth/providers/google'
 
-   export const { handlers, auth, signIn, signOut } = NextAuth({
-       ...authConfig,
-       providers: [
-           Google({
-              clientId: process.env.GOOGLE_CLIENT_ID!,
-              clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-           }),
-       ],
-   });
+const handlers = NextAuth({
+  ...authConfig,
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+})
 
-   export const { GET, POST } = handlers;
+export const GET = handlers
+export const POST = handlers
