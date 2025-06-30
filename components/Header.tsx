74300058517aa16576d13a623c65b21f3d226af6
Jonathan Piper <jonathan.piper@contentstack.com>
2025-06-30 15:08:29 -0700

    link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,24 @@
 'use client'
 
+import Link from 'next/link'
+
 export function Header() {
   return (
     <header className="bg-gray-800 p-4 text-white">
       <h1 className="text-4xl font-bold">IAB Use Case Analytics</h1>
       <p className="text-lg">
         Explore use cases categorized by{' '}
-        <a href="/analytics/authors" className="font-bold">
+        <Link href="/analytics/authors" className="font-bold">
           Author
-        </a>{' '}
+        </Link>{' '}
         |{' '}
-        <a href="/analytics/customers" className="font-bold">
+        <Link href="/analytics/customers" className="font-bold">
           Customer
-        </a>{' '}
+        </Link>{' '}
         |{' '}
-        <a href="/analytics/spaces" className="font-bold">
+        <Link href="/analytics/spaces" className="font-bold">
           Confluence Space
-        </a>
+        </Link>
       </p>
     </header>
   )
